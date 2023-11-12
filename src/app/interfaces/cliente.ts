eef019bb08c791bc716a8a88ae5a0f63847e36a8
AlexVelasquez98 <alexandrovelasquez84@gmail.com>
2023-11-11 22:37:51 -0500

    Fix: Login Cliente

--- src/app/interfaces/cliente.ts
+++ src/app/interfaces/cliente.ts
@@ -1,2 +1,16 @@
 export interface Cliente {
+  idCliente: number;
+  nombres: string;
+  apellidoPaterno: string;
+  apellidoMaterno: string;
+  numeroDocumento: string;
+  correo: string;
+  telefono: string;
+  direccion: string;
+  estado: number;
+  tipoDocumento: number;
+  usuarioCreacion: string;
+  fechaCreacion: Date;
+  usuarioActualizacion: string;
+  fechaActualizacion: Date;
 }
